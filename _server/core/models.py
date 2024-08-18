80980912e3a828b43e6dab00bbb1e210a27d52a6
kelson.gardner <kelson.gardner14@gmail.com>
2024-08-17 20:08:46 -0600

    Got a fundamental models.py file made with a profiel, library, deck, and car

--- _server/core/models.py
+++ _server/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_name = models.CharField(max_length=50, default='Anonymous')
+    email = models.CharField(max_length=50, default='Anonymous')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Library(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Deck(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=50, default=f'Deck {id}')
+    library = models.ForeignKey("Library", on_delete=models.CASCADE)
+
+class Card(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    frontSide = models.TextField()
+    backSide = models.TextField()
+    Deck = models.ForeignKey("Deck", on_delete=models.CASCADE)
