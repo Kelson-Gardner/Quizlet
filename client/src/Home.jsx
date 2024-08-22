cbf0de21abcc219dc7cd45e7099ab12a6ffbc7d7
kelson.gardner <kelson.gardner14@gmail.com>
2024-08-21 23:38:10 -0600

    Found a component for the recent flashcards section on the home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,12 +1,32 @@
+import { BorderAllRounded } from '@mui/icons-material';
 import './PageContents.css';
+import { Box, Paper } from '@mui/material';
+
 
 function Home(props){
 
     return(
         <>
-        <div className = {props.isOpen ? "is-open" : "not-open"} >
-            Your Decks
-        </div>
+        <h3 className = {props.isOpen ? "is-open" : "not-open"}>
+            Recent
+        </h3>
+        <Box
+        sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+            m: 1,
+            width: '20rem',
+            height: '10rem',
+            backgroundColor: 'rgb(175,230,230,.2)',
+            marginLeft: '150px',
+            borderRadius: '8px'
+            },
+        }}
+        >
+            {/* These will be the recent fashcard sets that the user has visited */}
+        <Paper elevation={3} />
+        </Box>
         </>
     )
 }
